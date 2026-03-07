d2d4bcdc9b6c4776edcf27f7c55ef56f12982cd7
Ashutosh kumbhar <ashutoshkumbhar1611@gmail.com>
2026-03-07 15:02:57 +0530

    Removed node_modules from repository

--- BACKEND/config/db.js
+++ BACKEND/config/db.js
@@ -3,7 +3,28 @@
 const mongoose = require('mongoose');
 
 const connectDB = async ()=>{
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("Mongo DB Connected");
+    try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error(err);
+  }
 }
+
+const db = mongoose.connection;
+
+//define event listeners for connect not connect
+db.on('connected',()=>{
+    console.log("Connected to mongodb server");
+});
+db.on('error',()=>{
+    console.log("mongodb Connection error");
+});
+db.on('disconnected',()=>{
+    console.log("disConnected from mongodb server");
+});
+
+module.exports = db;
+
+
 module.exports = connectDB;
--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const app = express();//app is like blueprint instance for server creation
 const db = require("./config/db");//add always to start
+const connectDB = require("./config/db");
 
+require("dotenv").config();
 
+const bodyparser = require("body-parser");
+app.use(bodyparser.json());//stores in req.body
+
+//connect to MongoDB
+connectDB();
 app.get('/',(req, res) => {
   res.send('Hello Users Welcome to our Resume Craft')
 })
 
-const PORT = process.env.PORT ||3000;
+const PORT = process.env.PORT ||5000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
 });
