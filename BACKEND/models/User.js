1779608ec3cdb56a9e5106ffb8aa7ab18aa18ba0
Ashutosh kumbhar <ashutoshkumbhar1611@gmail.com>
2026-03-12 22:12:45 +0530

    user routes added

--- BACKEND/models/User.js
+++ BACKEND/models/User.js
@@ -1,11 +1,13 @@
-//Authentication user schema
-
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   name:{
     type:String,
     required:true
+  },
+  username:{
+    type:String,
+    required:true,
   },
   email:{
     type:String,
--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();//app is like blueprint instance for server creation
 const connectDB = require("./config/db");
+const cors = require("cors");
 
 require("dotenv").config();
 
 //Middleware
+app.use(cors());
 app.use(express.json());
 
 //connect to MongoDB
@@ -14,11 +16,19 @@ connectDB();
 const resumeRoutes = require('./routes/resumeRoutes');
 app.use('/api/resume', resumeRoutes); 
 
+//import user router for auth
+const userRoutes = require("./routes/userRoutes");
+app.use('/api/users',userRoutes);
+
 app.get('/',(req, res) => {
   res.send('Hello Users Welcome to our Resume Craft')
 })
 
-const PORT = process.env.PORT ||5000;
+const PORT = process.env.PORT ||3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
 });
+
+
+
+
